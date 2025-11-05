3bdbee3d9cd12943a03abf8ab069724315181d02
Kilian Testard <pc47xiy@eduvaud.ch>
2025-11-05 12:14:43 +0100

    feat: finished exercise 2.1

--- test_prix.py
+++ test_prix.py
@@ -1,6 +1,40 @@
 import pytest
 from prix import calculer_prix_ttc
 
+
+def test_calculer_prix_ttc_standard():
+    # Arrange
+    prix_ht, tva = 100, 0.2
+    # Act
+    expected = calculer_prix_ttc(prix_ht, tva)
+    # Assert
+    assert expected == 120
+
+def test_calculer_prix_ttc_with_negative_price():
+    # Arrange
+    prix_ht, tva = -50, 0.2
+    # Aact + Assert
+    with pytest.raises(ValueError):
+        calculer_prix_ttc(prix_ht, tva)
+
+def test_calculer_prix_ttc_with_negative_tva():
+    # Arrange
+    prix_ht, tva = 50, -0.2
+    # Act + Assert
+    with pytest.raises(ValueError):
+        calculer_prix_ttc(prix_ht, tva)
+
+def test_calculer_prix_ttc_with_0():
+    # Arrange
+    prix_ht, tva = 0, 0.2
+    # Act
+    expected = calculer_prix_ttc(prix_ht, tva)
+    # Assert
+    assert expected == 0
+
 def test_calculer_prix_ttc_invalid_type():
+    # Arrange
+    prix_ht, tva = "cent", 0.2
+    # Act + Assert
     with pytest.raises(TypeError):
-        calculer_prix_ttc
+        calculer_prix_ttc(prix_ht, tva)
